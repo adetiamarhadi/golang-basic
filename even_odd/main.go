6cf68efbaea6fdbf6ddd92b507ef583386c5d0d5
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-06-26 10:11:21 +0700

    assignment even odd

---
+++ even_odd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	numbers := []int{}
+
+	for i := 0; i <= 10; i++ {
+		numbers = append(numbers, i)
+	}
+
+	for _, number := range numbers {
+		if number%2 == 0 {
+			fmt.Println(number, "is even")
+		} else {
+			fmt.Println(number, "is odd")
+		}
+	}
+}
