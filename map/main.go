225ed19392e3f6209cb3b9600cf3329b294b4786
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-06-29 11:42:01 +0700

    manipulate map

--- map/main.go
+++ map/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	color := map[string]string{
-		"red":   "#ff0000",
-		"white": "#ffffff",
-	}
+	// var color map[string]string
+
+	color := make(map[string]string)
+
+	color["red"] = "merah"
+
+	// color := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"white": "#ffffff",
+	// }
+
+	delete(color, "red")
 
 	fmt.Println(color)
 }
