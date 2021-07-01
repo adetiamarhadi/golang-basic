60b2ee76fbd93f1c532685b86c297759b6724e1d
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-07-01 15:22:39 +0700

    repeating routines

--- channels/main.go
+++ channels/main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		checkLink(<-c, c)
 	}
 }
 
@@ -29,10 +29,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down")
-		c <- "Sad!"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Good!"
+	c <- link
 }
